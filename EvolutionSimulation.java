6b4493445ed9fe730a9be840ed2a095bf912c094
Adin Bartoli <bartolia@ensipc229.ensimag.fr>
2025-01-17 15:11:51 +0100

    commit

--- EvolutionSimulation.java
+++ EvolutionSimulation.java
@@ -3,27 +3,11 @@ import java.awt.*;
 import java.util.Random;
 import java.util.List;
 
-public static ArrayList<Double> normalizeProbas(ArrayList<Double> probas) {
-    // Create a copy of the probas list
-    ArrayList<Double> normalizedProbas = new ArrayList<>(probas);
-    
-    // Calculate the sum of the probabilities
-    double sum = 0;
-    for (double proba : probas) {
-        sum += proba;
-    }
-    
-    // Normalize each probability and store it in normalizedProbas
-    for (int i = 0; i < probas.size(); i++) {
-        double normalized = probas.get(i) / sum;
-        normalizedProbas.set(i, normalized);
-    }
-    
-    return normalizedProbas;
-}
 
 class Individual {
+    private int type;
     private double[] phenotype;
+    private float fitness;
 
     public Individual(int traitCount) {
         phenotype = new double[traitCount];
@@ -33,8 +17,11 @@ class Individual {
         }
     }
 
-    public double[] getPhenotype() {
-        return phenotype;
+    public void randomizePhenotype() {
+        Random random = new Random();
+        for (int i = 0; i < phenotype.length; i++) {
+            phenotype[i] = random.nextDouble(); // Random phenotype value between 0 and 1
+        }
     }
 
     public Color getColor() {
@@ -43,12 +30,13 @@ class Individual {
     }
 }
 
-class GridPanel extends JPanel {
+
+class Population extends JPanel {
     private final int gridSize; // Number of cells in one dimension
     private final int cellSize; // Size of each cell in pixels
     private final Individual[][] grid;
 
-    public GridPanel(int gridSize, int cellSize, int traitCount) {
+    public Population(int gridSize, int cellSize, int traitCount) {
         this.gridSize = gridSize;
         this.cellSize = cellSize;
         this.grid = new Individual[gridSize][gridSize];
@@ -80,8 +68,12 @@ class GridPanel extends JPanel {
     }
 
     public void updateGrid() {
-        // Mutation logic can be added here
-        repaint();
+        // Randomize the phenotype of a random individual to simulate change
+        Random random = new Random();
+        int x = random.nextInt(gridSize);
+        int y = random.nextInt(gridSize);
+        grid[x][y].randomizePhenotype();
+        repaint(); // Repaint the panel to reflect updates
     }
 }
 
@@ -93,14 +85,25 @@ public class EvolutionSimulation {
         int cellSize = Math.min(screenSize.width, screenSize.height) / gridSize; // Calculate cell size to fit the screen
 
         JFrame frame = new JFrame("Evolution Simulation");
-        GridPanel gridPanel = new GridPanel(gridSize, cellSize, 3);
+        Population pop = new Population(gridSize, cellSize, 3);
 
-        frame.add(gridPanel);
+
+
+        frame.add(pop);
         frame.pack(); // Automatically sizes the frame to fit the panel
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null); // Center the frame on the screen
         frame.setVisible(true);
 
-        new Timer(1000, e -> gridPanel.updateGrid()).start();
+        int n = 5000; // Set the number of random updates you want
+        for (int i = 0; i < n; i++) {
+            // Sleep for a brief time to simulate updates over time
+            try {
+                Thread.sleep(100); // Delay 100 ms between updates
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            pop.updateGrid(); // Randomly update the grid
+        }
     }
 }
